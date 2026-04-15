56aa93a458095d3db75fb84ac3e89c745cc5e871
Yosua Sinaga <yosuasinaga81@gmail.com>
2026-04-15 17:41:20 +0700

    Music MainMenu

--- Assets/script/Main menu/MenuManager.cs
+++ Assets/script/Main menu/MenuManager.cs
@@ -1,3 +1,4 @@
+﻿using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,24 +8,81 @@ public class MenuManager : MonoBehaviour
     public CinemachineCamera MenuCam;
     public CinemachineCamera CharacterSelectCam;
 
-    // Fungsi ini dipanggil lewat UI Button
+    // ?? BGM
+    public AudioSource bgmSource;
+    public AudioClip mainMenuBGM;
+    public AudioClip characterBGM;
+
+    public float fadeDuration = 0.5f;
+
+    // ?? SFX
+    public AudioSource sfxSource;
+    public AudioClip clickSFX;
+
+    void Start()
+    {
+        bgmSource.clip = mainMenuBGM;
+        bgmSource.volume = 1f;
+        bgmSource.Play();
+    }
+
+    public void SwitchMusic(AudioClip newClip)
+    {
+        StartCoroutine(FadeMusic(newClip));
+    }
+
+    IEnumerator FadeMusic(AudioClip newClip)
+    {
+        float startVolume = bgmSource.volume;
+
+        while (bgmSource.volume > 0)
+        {
+            bgmSource.volume -= startVolume * Time.deltaTime / fadeDuration;
+            yield return null;
+        }
+
+        bgmSource.Stop();
+        bgmSource.clip = newClip;
+        bgmSource.Play();
+
+        while (bgmSource.volume < startVolume)
+        {
+            bgmSource.volume += startVolume * Time.deltaTime / fadeDuration;
+            yield return null;
+        }
+    }
+
+    // ?? fungsi klik
+    void PlayClick()
+    {
+        sfxSource.PlayOneShot(clickSFX);
+    }
+
     public void SwitchToMenu()
     {
+        PlayClick();
+
         CameraManager.SwitchCamera(MenuCam);
+        SwitchMusic(mainMenuBGM);
     }
 
     public void SwitchToOption()
     {
+        PlayClick();
+
         CameraManager.SwitchCamera(CharacterSelectCam);
+        SwitchMusic(characterBGM);
     }
- 
+
     public void PlayGame()
     {
+        PlayClick();
         SceneManager.LoadScene("InGame");
     }
 
     public void QuitGame()
     {
+        PlayClick();
         Application.Quit();
         Debug.Log("Quit Game");
     }
